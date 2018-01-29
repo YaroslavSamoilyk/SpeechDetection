e3c22ed7c7768e2a4657c91ebd6eb875c19e6c88
Yaroslav <yaroslav.samoiluk@gmail.com>
2018-01-29 14:13:13 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+var p = document.createElement('p');
+var words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', function (e) {
+   var transcript = Array.from(e.results)
+       .map(function (result) {
+           return result[0];
+       })
+       .map(function (result) {
+           return result.transcript;
+       }).join('');
+   p.textContent = transcript;
+   if(e.results[0].isFinal) {
+       p = document.createElement('p');
+       words.appendChild(p);
+   }
+});
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
+
